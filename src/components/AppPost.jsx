81562859e6a34ffc335f2adddf76c73950b54635
DamianoCasolari <damianocasolari@gmail.com>
2023-12-01 15:54:19 +0100

    add colors to tags

--- src/components/AppPost.jsx
+++ src/components/AppPost.jsx
@@ -3,9 +3,16 @@ export function AppPost({ id, title, image, content, tags }) {
     return new URL("../assets/img/postImg/" + name, import.meta.url).href;
   }
 
+  const MapColor = {
+    html: " bg-sky-600",
+    css: " bg-amber-800",
+    js: " bg-red-950",
+    php: " bg-gray-400",
+  };
+
   return (
     <div key={id} className="card_container p-4 w-full md:w-6/12 xl:w-3/12">
-      <div className="card p-4 min-h-[300] h-full bg_carbon shadow-black shadow-xl rounded-2xl text-slate-50">
+      <div className="card p-4 min-h-[300px] h-full bg_carbon shadow-black shadow-xl rounded-2xl text-slate-50">
         <div className="h-48">
           <img
             src={getImgUrl(image + ".jpg")}
@@ -17,10 +24,16 @@ export function AppPost({ id, title, image, content, tags }) {
         <div className="overflow-auto max-h-[800px]">
           <p>{content}</p>
         </div>
-        <div className="my-3 font-bold text-[20px]">
+        <div className="mt-4 font-bold text-[20px] ">
           {tags.map((tag) => {
             return (
-              <span key={id} className={"me-2 "}>
+              <span
+                key={id}
+                className={
+                  "me-2 rounded-full bg-slate-400 py-1 px-6" +
+                  (MapColor[tag] || "bg-gray-500")
+                }
+              >
                 {tag}{" "}
               </span>
             );
--- src/components/AppPostList.jsx
+++ src/components/AppPostList.jsx
@@ -2,11 +2,23 @@ import { posts } from "../store";
 import { AppPost } from "./AppPost";
 
 export function AppPostList() {
+  function createPostList() {
+    return posts.map((post) => {
+      return <AppPost {...post}></AppPost>;
+    });
+  }
+
   return (
     <div className="container pt-10 flex flex-wrap">
-      {posts.map((post) => {
-        return <AppPost {...post}></AppPost>;
-      })}
+      {createPostList().length > 0 ? (
+        createPostList()
+      ) : (
+        <div className="h-full w-full flex justify-center items-center">
+          <div className="card p-4 flex justify-center items-center text-[30px] h-full bg_carbon shadow-black shadow-xl rounded-2xl text-slate-50">
+            Nessun post Disponibile
+          </div>
+        </div>
+      )}
     </div>
   );
 }
